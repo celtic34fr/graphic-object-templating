68352aee3614c4af9b984b8fa6d278741a6c714f
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2016-06-12 22:02:16 +0200

    correction appel rendu Twig dan header (resté avec sl au lieux de _twigRender)

--- src/GraphicObjectTemplating/Service/GotServices.php
+++ src/GraphicObjectTemplating/Service/GotServices.php
@@ -54,49 +54,6 @@ class GotServices
 		return $renduHtml;
 	}
 
-    public function header($objets)
-    {
-        if (!is_array($objets)) $objets = array(0 => $objets);
-        $cssScripts = [];
-        $jsScripts  = [];
-        $rscs       = "";
-        foreach ($objets as $objet) {
-            $properties = $objet->getProperties();
-            $rscs = $properties['resources'];
-            if (!empty($rscs) && ($rscs !== false)) {
-                $cssList = $rscs['css'];
-                $jsList  = $rscs['js'];
-                if (!empty($cssList)) {
-                    foreach ($cssList as $item) {
-                        if (!in_array($item, $cssScripts)) $cssScripts[] = 'graphicobjecttemplating/'.$item;
-                    }
-                }
-                if (!empty($jsList)) {
-                    foreach ($jsList as $item) {
-                        if (!in_array($item, $jsScripts)) $jsScripts[] = 'graphicobjecttemplating/'.$item;
-                    }
-                }
-            }
-
-            if ($objet instanceof OSContainer) {
-                $children = $objet->getChildren();
-                if (!empty($children)) {
-                    foreach ($children as $child) {
-                        $tmpArray = $this->headerChild($child);
-                        $cssScripts = array_merge($cssScripts, $tmpArray['css']);
-                        $jsScripts  = array_merge($jsScripts,  $tmpArray['js']);
-                    }
-                }
-            }
-        }
-
-        $view = new ViewModel();
-        $view->setTemplate('graphic-object-templating/got/got-header.twig');
-        $view->setVariable('scripts', array('css' => $cssScripts, 'js' => $jsScripts));
-        $renduHtml = $this->sl->get('ZfcTwigRenderer')->render($view);
-        return $renduHtml;
-    }
-
     public function bootstrapClass($widthBT)
     {
         if (is_numeric($widthBT)) {
@@ -134,7 +91,51 @@ class GotServices
         return $class;
     }
 
-    
+    public function header($objets)
+    {
+        $view = new ViewModel();
+        $cssScripts = [];
+        $jsScripts  = [];
+        $rscs       = "";
+        if (!is_array($objets)) $objets = array(0 => $objets);
+
+        foreach ($objets as $objet) {
+            $properties = $objet->getProperties();
+            $rscs = $properties['resources'];
+            if (!empty($rscs) && ($rscs !== false)) {
+                $cssList = $rscs['css'];
+                $jsList  = $rscs['js'];
+                if (!empty($cssList)) {
+                    foreach ($cssList as $item) {
+                        if (!in_array($item, $cssScripts)) $cssScripts[] = 'graphicobjecttemplating/'.$item;
+                    }
+                }
+                if (!empty($jsList)) {
+                    foreach ($jsList as $item) {
+                        if (!in_array($item, $jsScripts)) $jsScripts[] = 'graphicobjecttemplating/'.$item;
+                    }
+                }
+            }
+
+            if ($objet instanceof OSContainer) {
+                $children = $objet->getChildren();
+                if (!empty($children)) {
+                    foreach ($children as $child) {
+                        $tmpArray = $this->headerChild($child);
+                        $cssScripts = array_merge($cssScripts, $tmpArray['css']);
+                        $jsScripts  = array_merge($jsScripts,  $tmpArray['js']);
+                    }
+                }
+            }
+        }
+
+        $view->setTemplate('graphic-object-templating/got/got-header.twig');
+        $view->setVariable('scripts', array('css' => $cssScripts, 'js' => $jsScripts));
+        $renduHtml = $this->_twigRender->render($view);
+        return $renduHtml;
+    }
+
+
 
     private function headerChild($objet)
     {
