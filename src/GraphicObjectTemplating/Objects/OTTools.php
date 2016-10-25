6ffcf657871ad9bce981494bd71414a208d2f425
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2016-10-25 17:42:30 +0200

    complément objet fichier manquant

---
+++ src/GraphicObjectTemplating/Objects/OTTools.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gilbert
+ * Date: 25/02/16
+ * Time: 10:42
+ */
+
+namespace GraphicObjectTemplating\Objects;
+
+use GraphicObjectTemplating\Objects\OObject;
+
+/**
+ * Class OSContainer
+ * @package GraphicObjectTemplating\Objects
+ *
+ */
+class OTTools extends OObject
+{
+
+    public function __construct($id, $adrProperties)
+    {
+        $properties = include(__DIR__ ."/../../../view/graphic-object-templating/oobject/ottools/ottools.config.phtml");
+        parent::__construct($id, $adrProperties);
+        $properties = array_merge($this->getProperties(), $properties);
+        $this->setProperties($properties);
+    }
+
+}
