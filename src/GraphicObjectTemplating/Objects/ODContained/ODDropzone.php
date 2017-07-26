c67e98ce67545c15ccac95cc1f8cdd9d32e8cac0
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2017-07-26 22:41:17 +0200

    développement ODDropzone

--- src/GraphicObjectTemplating/Objects/ODContained/ODDropzone.php
+++ src/GraphicObjectTemplating/Objects/ODContained/ODDropzone.php
@@ -1,16 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: candidat
- * Date: 26/07/17
- * Time: 13:58
- */
 
-namespace GraphicObjectTemplating\src\GraphicObjectTemplating\Objects\ODContained;
-
+namespace GraphicObjectTemplating\Objects\ODContained;
 
 use GraphicObjectTemplating\Objects\ODContained;
 
+/**
+ * Class ODDropzone
+ * @package GraphicObjectTemplating\Objects\ODContained
+ *
+ * addFilter($name, $filter)
+ * rmFilter($name)
+ * setFilter($name, $filter)
+ * getFilter($name)
+ * getFilters()
+ * enaMultiple()
+ * disMultiple()
+ */
 class ODDropzone extends ODContained
 {
     public function __construct($id) {
@@ -18,6 +23,96 @@ class ODDropzone extends ODContained
         $this->setDisplay();
         $width = $this->getWidthBT();
         if (!is_array($width) || empty($width)) $this->setWidthBT(12);
+        return $this;
+    }
+
+    public function addFilter($name, $filter)
+    {
+        $name   = (string) $name;
+        $filter = (string) $filter;
+        $properties = $this->getProperties();
+        $filters    = (isset($properties['filters']) && !empty($properties['filters'])) ? $properties['filters'] : array();
+
+        if (!array_key_exists($name, $filters)) {
+            $filters[$name] = $filter;
+
+            $properties['filters'] = $filters;
+            $this->setProperties( $properties );
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function rmFilter($name)
+    {
+        $name       = (string) $name;
+        $properties = $this->getProperties();
+        $filters    = (isset($properties['filters']) && !empty($properties['filters'])) ? $properties['filters'] : array();
+
+        if (!array_key_exists($name, $filters)) {
+            unset($filters[$name]);
+
+            $properties['filters'] = $filters;
+            $this->setProperties( $properties );
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setFilter($name, $filter)
+    {
+        $name   = (string) $name;
+        $filter = (string) $filter;
+        $properties = $this->getProperties();
+        $filters    = (isset($properties['filters']) && !empty($properties['filters'])) ? $properties['filters'] : array();
+
+        $filters[$name] = $filter;
+
+        $properties['filters'] = $filters;
+        $this->setProperties( $properties );
+        return true;
+    }
+
+    public function setFilters(array $filters)
+    {
+        $properties = $this->getProperties();
+        $properties['filters'] = $filters;
+        $this->setProperties($properties);
+        return $this;
+    }
+
+    public function getFilter($name)
+    {
+        $name   = (string) $name;
+        $properties = $this->getProperties();
+
+        if (array_key_exists($name, $properties['filters'])) {
+            return $properties['filters'][$name];
+        }
+        return false;
+    }
+
+    public function getFilters()
+    {
+        $properties = $this->getProperties();
+        return (isset($properties['filters']) && !empty($properties['filters'])) ? $properties['filters'] : array();
+    }
+
+    public function enaMultiple()
+    {
+        $properties = $this->getProperties();
+        $properties['multiple'] = true;
+        $this->setProperties($properties);
+        return $this;
+    }
+
+    public function disMultiple()
+    {
+        $properties = $this->getProperties();
+        $properties['multiple'] = false;
+        $this->setProperties($properties);
         return $this;
     }
 }
--- view/graphic-object-templating/oobject/odcontained/oddropzone/oddropzone.config.php
+++ view/graphic-object-templating/oobject/odcontained/oddropzone/oddropzone.config.php
@@ -1,13 +1,15 @@
 <?php
 return array(
-    "object"     => "oddropzone",
-    "typeObj"    => "odcontained",
-    "template"   => "oddropzone.twig",
+    'object'     => 'oddropzone',
+    'typeObj'    => 'odcontained',
+    'template'   => 'oddropzone.twig',
+    'filters'    => array(),
+    'multiple'   => false,
 
-    "resources" => array(
-        "css" => array( 'dropzone.css' => 'css/dropzone.css' ),
-        "js"  => array( 'dropzone.js' => 'js/dropzone.js' ),
-        "img" => array( 
+    'resources' => array(
+        'css' => array( 'dropzone.css' => 'css/dropzone.css' ),
+        'js'  => array( 'dropzone.js' => 'js/dropzone.js' ),
+        'img' => array(
 			'spritemap.png' => 'img/spritemap.png',
 			'spritemap@2x.png' => 'img/spritemap@2x.png',
         )
