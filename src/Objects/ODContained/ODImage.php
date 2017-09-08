b4985b624570341e65071635894119ead82b7110
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2017-09-08 11:41:53 +0200

    ajout méthode alimentation restitution des attributs de l'objet ODImage

--- src/Objects/ODContained/ODImage.php
+++ src/Objects/ODContained/ODImage.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: candidat
- * Date: 08/09/17
- * Time: 09:24
- */
 
 namespace GraphicObjectTemplating\Objects\ODContained;
 
@@ -18,4 +12,83 @@ class ODImage extends ODContained
         $this->setDisplay();
     }
 
+    public function setUrl($url)
+    {
+        $url                 = (string) $url;
+        $properties          = $this->getProperties();
+        $properties['url']   = $url;
+        $this->setProperties($properties);
+        return $this;
+    }
+
+    public function getUrl()
+    {
+        $properties          = $this->getProperties();
+        return ((!empty($properties['url'])) ? $properties['url'] : false) ;
+    }
+
+    public function setAlt($alt)
+    {
+        $alt                 = (string) $alt;
+        $properties          = $this->getProperties();
+        $properties['alt']   = $alt;
+        $this->setProperties($properties);
+        return $this;
+    }
+
+    public function getAlt()
+    {
+        $properties          = $this->getProperties();
+        return ((!empty($properties['alt'])) ? $properties['alt'] : false) ;
+    }
+
+    public function setWidth($width)
+    {
+        $width               = (int) $width;
+        $properties          = $this->getProperties();
+        $properties['width'] = $width;
+        $this->setProperties($properties);
+        return $this;
+    }
+
+    public function getWidth()
+    {
+        $properties          = $this->getProperties();
+        return ((!empty($properties['width'])) ? $properties['width'] : false) ;
+    }
+
+    public function setHeight($height)
+    {
+        $height               = (int) $height;
+        $properties           = $this->getProperties();
+        $properties['height'] = $height;
+        $this->setProperties($properties);
+        return $this;
+    }
+
+    public function getHeight()
+    {
+        $properties          = $this->getProperties();
+        return ((!empty($properties['height'])) ? $properties['height'] : false) ;
+    }
+
+    public function evtClick($class, $method, $stopEvent = true)
+    {
+        $properties = $this->getProperties();
+        if(!isset($properties['event'])) $properties['event'] = [];
+        $properties['event']['click'] = [];
+        $properties['event']['click']['class'] = $class;
+        $properties['event']['click']['method'] = $method;
+        $properties['event']['click']['stopEvent'] = ($stopEvent) ? 'OUI' : 'NON';
+        $this->setProperties($properties);
+        return $this;
+    }
+
+    public function disClick()
+    {
+        $properties             = $this->getProperties();
+        if (isset($properties['event']['click'])) unset($properties['event']['click']);
+        $this->setProperties($properties);
+        return $this;
+    }
 }
