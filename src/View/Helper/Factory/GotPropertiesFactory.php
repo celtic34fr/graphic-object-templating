39f5d8ba4c88f2be6623d10f803ea2774cdbd601
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2017-09-21 14:02:42 +0200

    mise en place coquille pour viewHelper de restitution des properties d'un objet

---
+++ src/View/Helper/Factory/GotPropertiesFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace GraphicObjectTemplating\View\Helper\Factory;
+
+use Interop\Container\ContainerInterface;
+use Interop\Container\Exception\ContainerException;
+use Zend\ServiceManager\Exception\ServiceNotCreatedException;
+use Zend\ServiceManager\Exception\ServiceNotFoundException;
+use Zend\ServiceManager\Factory\FactoryInterface;
+use GraphicObjectTemplating\View\Helper\GotProperties;
+use Zend\ServiceManager\ServiceManager;
+
+class GotPropertiesFactory implements FactoryInterface
+{
+    /**
+     * Create an object
+     *
+     * @param  ContainerInterface $container
+     * @param  string $requestedName
+     * @param  null|array $options
+     * @return object
+     * @throws ServiceNotFoundException if unable to resolve the service.
+     * @throws ServiceNotCreatedException if an exception is raised when
+     *     creating a service.
+     * @throws ContainerException if any other error occurs
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        /** @var ServiceManager $serviceManager */
+        $serviceManager =  $container->get('ServiceManager');
+        return new GotProperties($serviceManager);
+    }
+}
---
+++ src/View/Helper/GotProperties.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace GraphicObjectTemplating\View\Helper;
+
+use Zend\ServiceManager\ServiceManager;
+use Zend\View\Helper\AbstractHelper;
+
+class GotProperties extends AbstractHelper
+{
+    protected $sm;
+    protected $gotServices;
+
+    public function __construct($sm)
+    {
+        /** @var ServiceManager sl */
+        $this->sm = $sm;
+        $this->gotServices = $sm->get("graphic.object.templating.services");
+        return $this;
+    }
+
+    public function __invoke($object)
+    {
+    }
+}
