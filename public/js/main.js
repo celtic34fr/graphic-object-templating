7a223b1780f0380df3794e48a9ebed5b9a7d7c41
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2017-10-18 16:43:59 +0200

    tentative de correction méthode hasAttr()

--- public/js/main.js
+++ public/js/main.js
@@ -589,7 +589,7 @@ String.prototype.replaceAll = function (str1, str2, ignore) {
     return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g, "\\$&"), (ignore ? "gi" : "g")), (typeof(str2) == "string") ? str2.replace(/\$/g, "$$$$") : str2);
 }
 
-String.prototype.hasAttr = function(name) {
+Object.prototype.hasAttr = function(name) {
     return this.attr(name) !== undefined;
 };
 
